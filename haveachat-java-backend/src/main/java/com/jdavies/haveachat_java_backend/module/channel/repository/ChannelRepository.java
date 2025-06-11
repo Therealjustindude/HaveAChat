e9f752e39052a6eb6f96788c35f5aeaf17ceeeb6
Justin Davies <therealjustindude@gmail.com>
2025-06-11 14:11:54 -0400

    look to see if channel exists by name in repository

--- haveachat-java-backend/src/main/java/com/jdavies/haveachat_java_backend/module/channel/repository/ChannelRepository.java
+++ haveachat-java-backend/src/main/java/com/jdavies/haveachat_java_backend/module/channel/repository/ChannelRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface ChannelRepository extends JpaRepository<Channel, Long> {
         Optional<Channel> findByName(String name);
+        boolean existsByName(String name);
 }
