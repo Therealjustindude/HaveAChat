639c82b1d2ee37a9cd02cc7f7fb500b31c458920
Justin Davies <therealjustindude@gmail.com>
2025-06-12 14:50:02 -0400

    initial work for adding currentuser argument resolver - easier to identify logged in user through context

---
+++ haveachat-java-backend/src/main/java/com/jdavies/haveachat_java_backend/common/CurrentUserArgumentResolver.java
@@ -0,0 +1,41 @@
+package com.jdavies.haveachat_java_backend.common;
+
+import com.jdavies.haveachat_java_backend.common.annotation.CurrentUser;
+import com.jdavies.haveachat_java_backend.module.exception.CustomException;
+import com.jdavies.haveachat_java_backend.module.exception.ErrorType;
+import com.jdavies.haveachat_java_backend.module.user.model.User;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(CurrentUser.class) != null
+                && parameter.getParameterType().equals(User.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication instanceof UsernamePasswordAuthenticationToken token) {
+            Object principal = token.getPrincipal();
+            if (principal instanceof User user) {
+                return user;
+            }
+        }
+        throw new CustomException(ErrorType.UNAUTHORIZED, "User not authenticated");
+    }
+}
---
+++ haveachat-java-backend/src/main/java/com/jdavies/haveachat_java_backend/common/annotation/CurrentUser.java
@@ -0,0 +1,11 @@
+package com.jdavies.haveachat_java_backend.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CurrentUser {
+}
