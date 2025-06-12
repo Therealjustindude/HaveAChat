f6e3aa5d2962948f49008a2c5fe6dd341d9c5b1d
Justin Davies <therealjustindude@gmail.com>
2025-06-12 18:37:55 -0400

    added authenitaced user check to websocket controller and a delte func for websocket handler

--- haveachat-java-backend/src/main/java/com/jdavies/haveachat_java_backend/module/chat/websocket/ChatMessageHandler.java
+++ haveachat-java-backend/src/main/java/com/jdavies/haveachat_java_backend/module/chat/websocket/ChatMessageHandler.java
@@ -1,6 +1,7 @@
 package com.jdavies.haveachat_java_backend.module.chat.websocket;
 
 import com.jdavies.haveachat_java_backend.module.chat.dto.ChatMessageDTO;
+import com.jdavies.haveachat_java_backend.module.chat.model.ChatMessage;
 import com.jdavies.haveachat_java_backend.module.chat.service.ChatMessageService;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,10 @@ public class ChatMessageHandler {
     private final SimpMessagingTemplate messagingTemplate;
     private final ChatMessageService chatService;
 
-    public ChatMessageHandler(SimpMessagingTemplate messagingTemplate, ChatMessageService chatService) {
+    public ChatMessageHandler(
+            SimpMessagingTemplate messagingTemplate,
+            ChatMessageService chatService
+    ) {
         this.messagingTemplate = messagingTemplate;
         this.chatService = chatService;
     }
@@ -19,5 +23,16 @@ public class ChatMessageHandler {
     public void process(ChatMessageDTO dto, String sessionId) {
         ChatMessageDTO saved = this.chatService.saveChatMessage(dto); // Save to DB
         messagingTemplate.convertAndSend("/topic/channel/" + dto.getChannelId(), saved);
+    }
+
+    public void delete(Long messageId) {
+        ChatMessage message = chatService.getByChatMessageIdOrThrow(messageId);
+        chatService.deleteMessage(messageId);
+
+        // Notify all subscribers
+        messagingTemplate.convertAndSend(
+                "/topic/channel/" + message.getChannelId() + "/messages/deleted",
+                messageId
+        );
     }
 }
--- haveachat-java-backend/src/main/java/com/jdavies/haveachat_java_backend/module/chat/websocket/ChatWebSocketController.java
+++ haveachat-java-backend/src/main/java/com/jdavies/haveachat_java_backend/module/chat/websocket/ChatWebSocketController.java
@@ -1,25 +1,64 @@
 package com.jdavies.haveachat_java_backend.module.chat.websocket;
 
 import com.jdavies.haveachat_java_backend.module.chat.dto.ChatMessageDTO;
+import com.jdavies.haveachat_java_backend.module.exception.CustomException;
+import com.jdavies.haveachat_java_backend.module.exception.ErrorType;
+import com.jdavies.haveachat_java_backend.module.user.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.security.Principal;
 
 @Controller
 public class ChatWebSocketController {
 
     private final ChatMessageHandler handler;
 
-    public ChatWebSocketController(ChatMessageHandler handler) {
+    private final UserService userService;
+
+    public ChatWebSocketController(ChatMessageHandler handler, UserService userService) {
         this.handler = handler;
+        this.userService = userService;
     }
 
     @MessageMapping("/chat.sendMessage") // /app/chat.sendMessage
     public void sendMessage(@Payload ChatMessageDTO message,
-                            @Header("simpSessionId") String sessionId) {
+                            @Header("simpSessionId") String sessionId,
+                            Principal principal) {
+        // Assert user is authenticated
+        String userEmail =  getUserEmailFromPrincipalOrThrow(principal);
+        // Assert user exists
+        assertUserExistsByEmail(userEmail);
+
         handler.process(message, sessionId);
     }
 
+    @DeleteMapping("/messages/{id}")
+    public ResponseEntity<?> deleteMessage(@PathVariable Long id, Principal principal) {
+        // Assert user is authenticated
+        String userEmail =  getUserEmailFromPrincipalOrThrow(principal);
+        // Assert user exists
+        assertUserExistsByEmail(userEmail);
 
+        handler.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    private String getUserEmailFromPrincipalOrThrow(Principal principal) {
+        if (principal == null || principal.getName() == null || principal.getName().isEmpty()) {
+            throw new CustomException(ErrorType.UNAUTHORIZED, "User is not authenticated");
+        }
+        return principal.getName();
+    }
+
+    private void assertUserExistsByEmail(String userEmail) {
+        if(!userService.existsByEmail(userEmail)) {
+            throw new CustomException(ErrorType.NOT_FOUND, "User not found");
+        }
+    }
 }
