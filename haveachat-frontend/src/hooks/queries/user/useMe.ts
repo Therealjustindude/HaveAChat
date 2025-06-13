5ad3c54b3f963780cf467267633d3e0655b94081
Justin Davies <therealjustindude@gmail.com>
2025-06-13 16:09:35 -0400

    UserDto type added to auth provider and useMe hook

--- haveachat-frontend/src/hooks/queries/user/useMe.ts
+++ haveachat-frontend/src/hooks/queries/user/useMe.ts
@@ -1,17 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
 import { API_BASE_URL } from '@haveachat/utils/ApiBaseUrl';
 import { fetchWithAuth } from '@haveachat/hooks/utils/fetchWithAuth';
+import { UserDTO } from '@haveachat/api';
 
-
-
-const fetchMe = async () => {
+const fetchMe = async (): Promise<UserDTO> => {
   const res = await fetchWithAuth(`${API_BASE_URL}/api/user`);
   if (!res.ok) throw new Error('Unauthorized');
-  return res.json();
+  return res.json() as Promise<UserDTO>; // or use await res.json() with a cast
 };
 
 export const useMe = () => {
-  return useQuery({
+  return useQuery<UserDTO>({
     queryKey: ['me'],
     queryFn: fetchMe,
     retry: false,
