d21206901ad563d61db5e01e2ca6e0f9a9e97c6b
Justin Davies <therealjustindude@gmail.com>
2025-06-16 00:06:44 -0400

    frontend - get channels and chat history hooks

---
+++ haveachat-frontend/src/hooks/queries/channel/useGetChannels.ts
@@ -0,0 +1,19 @@
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { API_BASE_URL } from '@haveachat/utils/ApiBaseUrl';
+import { fetchWithAuth } from '@haveachat/hooks/utils/fetchWithAuth';
+import { Channel, UserDTO } from '@haveachat/api';
+
+const fetchChannels = async (): Promise<Array<Channel>> => {
+	const res = await fetchWithAuth(`${API_BASE_URL}/api/channels`);
+	if (!res.ok) throw new Error('Unauthorized');
+	return res.json() as Promise<Array<Channel>>;
+};
+
+export const useGetChannels = (options?: Partial<UseQueryOptions<Array<Channel>>>) => {
+	return useQuery<Array<Channel>>({
+		queryKey: ['channels'],
+		queryFn: fetchChannels,
+		retry: false,
+		...options,
+	});
+};
