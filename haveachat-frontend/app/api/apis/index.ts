bb6cf639e5f12631e322588d85a1816d1c69b0bf
Justin Davies <therealjustindude@gmail.com>
2025-06-18 09:47:05 -0400

    update open api specs

--- haveachat-frontend/app/api/apis/index.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './AuthControllerApi';
-export * from './ChannelControllerApi';
-export * from './ChatMessageControllerApi';
-export * from './UserControllerApi';
